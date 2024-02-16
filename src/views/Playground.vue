0a516bd0b12908f51181e627cf00f83638e29bb0
Tijmen Roes <tijmen@cre8ion.com>
2024-02-16 14:48:37 +0100

    added functionality to add custom filter words

--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -2,5 +2,31 @@
   <div>
     <h1>Playground</h1>
     <p>Playground</p>
+
+    <q-select
+      filled
+      v-model="filterWords"
+      multiple
+      use-chips
+      hide-dropdown-icon
+      use-input
+      new-value-mode="add"
+      label="Multiple selection"
+    />
+
+    <MessageWithWord
+      :data="data"
+      :filter-words="filterWords"
+    />
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import MessageWithWord from '../components/MessageWithWord.vue'
+import { useStore } from '../store'
+
+const store = useStore()
+const filterWords = ref(['word'])
+const data = computed(() => store.authorsDataMessages)
+</script>

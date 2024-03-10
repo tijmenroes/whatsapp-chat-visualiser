3819da38e2ff4901310b53795ad5909a3c9ff93b
tijmen <tijmen@cre8ion.com>
2024-03-10 19:52:49 +0100

    Revert "Revert "pushing dist to git aswell for gh-pages""
    This reverts commit bea41b28e2dc3e63035e4725464efdce509fea00.

--- src/utils/divideNumber.ts
+++
@@ -1,7 +0,0 @@
-export function divideNumber(initial: number, divideAmount: number) {
-  console.log(initial)
-  //   parseFloat((item / participant.messages.length).toFixed(2)))
-
-  //   console.log((initial / divideAmount).toFixed(2))
-  return parseFloat(((initial / divideAmount) * 100).toFixed(2))
-}
